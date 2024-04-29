79318a5fe6a0d6d629d4b866545eeaebb2424c03
Peter Mathis <peter.mathis@kombinat.at>
2024-04-29 12:16:27 +0200

    update description

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 setup(
     name="collective.contactformprotection",
     version="1.0a1",
-    description="This package protects the default contact form of Plone which is generally accessible via /contact-form. We add a (H/Re)captcha field depending on your installation and provide a checkbox in the controlpanel to disable it globally.",
+    description="This package protects the default contact form of Plone which is generally accessible via /contact-form. It provides a checkbox in the controlpanel to disable it globally and adds a (H/Re)captcha field depending on your installation.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
     classifiers=[
